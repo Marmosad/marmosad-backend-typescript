5998839e91fc9fe5feaca5b55ed2adc2f7efdd9d
Jonathan Yang <jonathan8589@gmail.com>
2018-06-24 19:34:06 -0400

    removed extra files

--- src/chatHandler.ts
+++
@@ -1,7 +0,0 @@
-import * as board '../board.js'
-import * as socketService from ('../dataServices/socketService')
-module.exports = {
-    onMessage: function (data, socketId) {
-        socketService.emit('message', {from: board.getPlayerName(socketId), msg: data});
-    }
-};
