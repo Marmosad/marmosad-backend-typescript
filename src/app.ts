c4c2edaa3af5f15f9ef167c0836db928e5c3395a
Wen Yu Ge <gewenyu99@gmail.com>
2019-02-16 17:54:49 -0500

    updated request format for boards generate and remove

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import "reflect-metadata"
-import BoardService, {} from "./service/boardService";
+import BoardService from "./service/boardService";
 import {container} from "./config/inversify.config";
 import {Http} from "./service/httpSingletonService";
 
@@ -7,13 +7,14 @@ export class App {
     private express = container.get<Http>(Http).express;
     private _port;
     private boardService: BoardService;
+
     constructor() {
         // Waits or server to boot.
-        container.get<Http>(Http).httpStart().then(()=>{
+        container.get<Http>(Http).httpStart().then(() => {
             console.log('server started successfully.');
             this.setupEndpoints();
             this.boardService = new BoardService();
-        }).catch((err)=>{
+        }).catch((err) => {
             console.log(err)
         });
     }
@@ -31,18 +32,18 @@ export class App {
         self.express.post('/boards/generate', function (req, res) {
             const nonRepeating = self.boardService.newBoard(req.body.name, req.body.playerLimit);
             if (nonRepeating) {
-                res.status(200).send("Successfully created " + req.body.name);
+                res.status(200).send({"message": "Successfully created " + req.body.name});
             } else {
-                res.status(400).send("unable to create board" + req.body.name);
+                res.status(400).send({"message": "unable to create board" + req.body.name});
             }
         });
 
         self.express.post('/boards/remove', function (req, res) {
             const exist = self.boardService.removeBoard(req.body.socketUrl);
             if (exist) {
-                res.status(200).send("Successfully removed " + req.body.socketUrl);
+                res.status(200).send({"message": "Successfully removed " + req.body.socketUrl});
             } else {
-                res.status(400).send("unable to delete board " + req.body.socketUrl);
+                res.status(400).send({"message": "unable to delete board " + req.body.socketUrl});
             }
         });
     }
