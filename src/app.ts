29605a21edb638e99e81ea532b3e0946034e8ac7
Wen Yu Ge <gewenyu99@gmail.com>
2019-05-27 18:45:00 -0400

    Added prestarts to firebase

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,23 @@ import "reflect-metadata"
 import BoardService from "./service/boardService";
 import {container} from "./config/inversify.config";
 import {Http} from "./service/httpSingletonService";
+import {FirebaseEndpoints, Pack, Response} from "./interface/firestoreInterface";
+import {FIREBASE_GET_BLACK_CARD, FIREBASE_GET_PACK, FIREBASE_GET_WHITE_CARD} from "./config/config";
+import * as rp from "request-promise-native"
 
 export class App {
     private express = container.get<Http>(Http).express;
     private _port;
     private boardService: BoardService;
+    private firebaseEndpoints: FirebaseEndpoints;
 
     constructor() {
         // Waits or server to boot.
+        this.firebaseEndpoints = {
+            getPack: FIREBASE_GET_PACK,
+            getBlackCard: FIREBASE_GET_BLACK_CARD,
+            getWhiteCard: FIREBASE_GET_WHITE_CARD,
+        }
         container.get<Http>(Http).httpStart().then(() => {
             console.log('server started successfully.');
             this.setupEndpoints();
@@ -26,6 +35,16 @@ export class App {
     private setupEndpoints() {
         const self = this;
         self.express.get('/boards', function (req, res) {
+            const options = {
+                method: 'GET',
+                uri: this.firebaseEndpoints.getBlackCard,
+                headers: {
+                    "card-pack-name": 'room-309',
+                    "card-id": 1
+                }
+            };
+
+            rp(options).then().catch();
             res.send(self.boardService.getBoardsInfo());
         });
 
